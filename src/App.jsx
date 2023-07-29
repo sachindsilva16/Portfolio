73b721a0d759cc2367a1d7f2b6df5c7131824be1
sachindsilva16 <letscode16@gmail.com>
2023-07-30 00:16:38 +0530

    Add header

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,13 @@ const App = () =>{
 
     return(
         <header className="header">
+
+            {/* Header Logo name */}
             <a href='#' className="logo">PortFolio</a>
 
-
+        {/* Navigation bar */}
             <nav className="navbar">
-                <a href='#home'>Home</a>
+                <a href='#home' className="active">Home</a>
                 <a href='#experience'>Experience</a>
                 <a href='#about'>About</a>
                 <a href='#projects'>Projects</a>
